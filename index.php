1f913e0dbf6c88d2ce273104e93d0ded958612dd
Tina Ostrander <tostrander@greenriver.edu>
2020-01-15 14:38:39 -0500

    Load Fat-free

--- index.php
+++ index.php
@@ -5,8 +5,20 @@
  * /328/chicken/index.php
  */
 
-session_start();
-
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+//Require autoload file
+require("vendor/autoload.php");
+
+//Instantiate F3
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo "Hello";
+});
+
+//Run F3
+$f3->run();
